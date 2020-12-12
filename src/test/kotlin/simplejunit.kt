65bfd42aed4ad41099ebb0851da77301882bd42c
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-12-12 12:45:03 +0100

    cd..

---
+++ src/test/kotlin/simplejunit.kt
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+
+class simplejunit {
+    @Test
+    internal fun function0() {
+        Assertions.assertEquals(10,10)
+    }
+
+    @Disabled("Test jeszcze nie został zaimpementowany")
+    @Test
+    internal fun function1() {
+        assert(true)
+    }
+}
