0457535dd3c50eae3c8341945698ac11a8922aa2
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-12-12 13:17:54 +0100

    cd..

--- src/test/kotlin/simplejunit.kt
+++ src/test/kotlin/simplejunit.kt
@@ -1,20 +1,30 @@
 import org.assertj.core.api.AssertionsForClassTypes.assertThat
 import org.assertj.core.api.AssertionsForClassTypes.within
+import org.assertj.core.data.Percentage
+import org.assertj.core.data.Percentage.withPercentage
 
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import java.lang.Math.pow
+import kotlin.math.exp
+import kotlin.math.sin
 
 class simplejunit {
     @Test
     internal fun function0() {
-        assertThat(10.0).isCloseTo(9.91, within(0.1))
+
+        assertThat(equation1(1.0)).isCloseTo(6.547, within(0.01))
+        assertThat(equation1(1.0)).isCloseTo(6.547, withPercentage(10.0))
     }
 
-    @Disabled("Test jeszcze nie został zaimpementowany")
     @Test
     internal fun function1() {
-        assert(true)
+        assertThat(equation1(1.0)).isEqualTo(6.547)
     }
 
-    //exp(x)^2-sin(x) dla x = 1
+    fun equation1(x: Double):Double{
+        //exp(x)^2-sin(x) dla x = 1
+        return pow(exp(x),2.0)-sin(x)
+    }
+
 }
